487dd7d81aaac39d444c76762b4d8d0550f3e307
vamsi2792 <60707566+vamsi2792@users.noreply.github.com>
2021-12-22 21:25:06 +0530

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -1,65 +1,241 @@
-# pip install streamlit fbprophet yfinance plotly
-import streamlit as st
-from datetime import date
+#!/usr/bin/env python
+# coding: utf-8
 
-import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
-
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
-st.title('Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+# In[2]:
 
 
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
+import pandas as pd
+import numpy as np
 
-	
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+get_ipython().run_line_magic('matplotlib', 'inline')
 
-st.subheader('Raw data')
-st.write(data.tail())
+# For reading stock data from yahoo
+from pandas_datareader.data import DataReader
 
-# Plot raw data
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
-	
-plot_raw_data()
+# For time stamps
+from datetime import datetime
 
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+# In[3]:
 
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
+
+# The tech stocks we'll use for this analysis
+tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+# Set up End and Start times for data grab
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+
+#For loop for grabing yahoo finance data and setting as a dataframe
+for stock in tech_list:   
+    # Set DataFrame as the Stock Ticker
+    globals()[stock] = DataReader(stock, 'yahoo', start, end)
+
+
+# In[6]:
+
+
+company_list = [AAPL, GOOG, MSFT, AMZN]
+company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
+for company, com_name in zip(company_list, company_name):
+    company["company_name"] = com_name
     
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+df = pd.concat(company_list, axis=0)
+df.tail(10)
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+
+# In[7]:
+
+
+# Summary Stats
+AAPL.describe()
+
+
+# In[8]:
+
+
+# General info
+AAPL.info()
+
+
+# In[9]:
+
+
+# Let's see a historical view of the closing price
+
+
+plt.figure(figsize=(15, 6))
+plt.subplots_adjust(top=1.25, bottom=1.2)
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company['Adj Close'].plot()
+    plt.ylabel('Adj Close')
+    plt.xlabel(None)
+    plt.title(f"Closing Price of {tech_list[i - 1]}")
+    
+plt.tight_layout()
+
+
+# In[10]:
+
+
+# Now let's plot the total volume of stock being traded each day
+plt.figure(figsize=(15, 7))
+plt.subplots_adjust(top=1.25, bottom=1.2)
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company['Volume'].plot()
+    plt.ylabel('Volume')
+    plt.xlabel(None)
+    plt.title(f"Sales Volume for {tech_list[i - 1]}")
+    
+plt.tight_layout()
+
+
+# In[11]:
+
+
+ma_day = [10, 20, 50]
+
+for ma in ma_day:
+    for company in company_list:
+        column_name = f"MA for {ma} days"
+        company[column_name] = company['Adj Close'].rolling(ma).mean()
+
+
+# In[12]:
+
+
+fig, axes = plt.subplots(nrows=2, ncols=2)
+fig.set_figheight(8)
+fig.set_figwidth(15)
+
+AAPL[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[0,0])
+axes[0,0].set_title('APPLE')
+
+GOOG[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[0,1])
+axes[0,1].set_title('GOOGLE')
+
+MSFT[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[1,0])
+axes[1,0].set_title('MICROSOFT')
+
+AMZN[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[1,1])
+axes[1,1].set_title('AMAZON')
+
+fig.tight_layout()
+
+
+# In[13]:
+
+
+# We'll use pct_change to find the percent change for each day
+for company in company_list:
+    company['Daily Return'] = company['Adj Close'].pct_change()
+
+# Then we'll plot the daily return percentage
+fig, axes = plt.subplots(nrows=2, ncols=2)
+fig.set_figheight(8)
+fig.set_figwidth(15)
+
+AAPL['Daily Return'].plot(ax=axes[0,0], legend=True, linestyle='--', marker='o')
+axes[0,0].set_title('APPLE')
+
+GOOG['Daily Return'].plot(ax=axes[0,1], legend=True, linestyle='--', marker='o')
+axes[0,1].set_title('GOOGLE')
+
+MSFT['Daily Return'].plot(ax=axes[1,0], legend=True, linestyle='--', marker='o')
+axes[1,0].set_title('MICROSOFT')
+
+AMZN['Daily Return'].plot(ax=axes[1,1], legend=True, linestyle='--', marker='o')
+axes[1,1].set_title('AMAZON')
+
+fig.tight_layout()
+
+
+# In[14]:
+
+
+# Note the use of dropna() here, otherwise the NaN values can't be read by seaborn
+plt.figure(figsize=(12, 7))
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    sns.distplot(company['Daily Return'].dropna(), bins=100, color='purple')
+    plt.ylabel('Daily Return')
+    plt.title(f'{company_name[i - 1]}')
+# Could have also done:
+#AAPL['Daily Return'].hist()
+plt.tight_layout()
+
+
+# In[15]:
+
+
+# Grab all the closing prices for the tech stock list into one DataFrame
+closing_df = DataReader(tech_list, 'yahoo', start, end)['Adj Close']
+
+# Let's take a quick look
+closing_df.head() 
+
+
+# In[16]:
+
+
+# Make a new tech returns DataFrame
+tech_rets = closing_df.pct_change()
+tech_rets.head()
+
+
+# In[17]:
+
+
+# Comparing Google to itself should show a perfectly linear relationship
+sns.jointplot('GOOG', 'GOOG', tech_rets, kind='scatter', color='seagreen')
+
+
+# In[18]:
+
+
+# We'll use joinplot to compare the daily returns of Google and Microsoft
+sns.jointplot('GOOG', 'MSFT', tech_rets, kind='scatter')
+
+
+# In[19]:
+
+
+# We can simply call pairplot on our DataFrame for an automatic visual analysis 
+# of all the comparisons
+
+sns.pairplot(tech_rets, kind='reg')
+
+
+# In[20]:
+
+
+# Set up our figure by naming it returns_fig, call PairPLot on the DataFrame
+return_fig = sns.PairGrid(tech_rets.dropna())
+
+# Using map_upper we can specify what the upper triangle will look like.
+return_fig.map_upper(plt.scatter, color='purple')
+
+# We can also define the lower triangle in the figure, inclufing the plot type (kde) 
+# or the color map (BluePurple)
+return_fig.map_lower(sns.kdeplot, cmap='cool_d')
+
+# Finally we'll define the diagonal as a series of histogram plots of the daily return
+return_fig.map_diag(plt.hist, bins=30)
+
+
+# In[ ]:
+
+
+
+
